113dd41ace9aa70dd8ec1eaa74d05d1da2cbbad9
Francisco <sedanfm@gmail.com>
2024-09-13 17:54:43 -0600

    Commit inicia

---
+++ demo1/src/main/java/test_1_java_maven/App.java
@@ -0,0 +1,16 @@
+package test_1_java_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static String[] args;
+
+    public static void main(String[] args )
+    {
+        App.args = args;
+        System.out.println( "Hello World!" );
+    }
+}
